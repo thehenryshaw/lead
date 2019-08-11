a0126f6621a808ca79130fa2bb2c616a842f5ba1
Igor Rodionov <henryshaw217@gmail.com>
2019-08-11 16:57:11 +0300

    Добавил отображение выбора меню

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const bars = document.querySelector('.burger');
 const showMenu = document.querySelector('.hide-menu');
 const languageSelection = document.querySelector('.language-selection');
 const showLang = document.querySelector('.hidden');
+const menuLinks = Array.from(document.querySelectorAll('.list-items a'));
 
 bars.addEventListener('click', () => {
   showMenu.classList.toggle('hide-menu');
@@ -15,10 +16,21 @@ window.addEventListener('resize', () => {
   }
 })
 
+
+
 languageSelection.addEventListener('mouseover', () => {
   showLang.classList.remove('hidden');
 })
 
 languageSelection.addEventListener('mouseleave', () => {
   showLang.classList.add('hidden');
+})
+
+menuLinks.forEach(el => {
+  el.addEventListener('click', (e) => {
+    menuLinks.forEach(el => {
+      el.classList.remove('chosen');
+    })
+    e.currentTarget.classList.add('chosen');
+  })
 })
