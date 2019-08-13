96e2a4d8a266d5b005a7dbea2021e60b8f19ccaa
Igor Rodionov <henryshaw217@gmail.com>
2019-08-13 14:47:34 +0300

    multiple changes

--- main.js
+++ main.js
@@ -5,6 +5,16 @@ const showMenu = document.querySelector('.hide-menu');
 const languageSelection = document.querySelector('.language-selection');
 const showLang = document.querySelector('.hidden');
 const menuLinks = Array.from(document.querySelectorAll('.list-items a'));
+const menu = document.querySelector('.list-container');
+
+const header = document.querySelector('.header');
+const showcaseSection = document.getElementById('showcase');
+const missionSection = document.getElementById('mission');
+const partnersSection = document.getElementById('partners');
+const reviewsSection = document.getElementById('reviews');
+const teamSection = document.getElementById('team');
+const workSection = document.getElementById('work');
+const contactsSection = document.getElementById('contacts');
 
 bars.addEventListener('click', () => {
   showMenu.classList.toggle('hide-menu');
@@ -26,6 +36,8 @@ languageSelection.addEventListener('mouseleave', () => {
   showLang.classList.add('hidden');
 })
 
+
+
 menuLinks.forEach(el => {
   el.addEventListener('click', (e) => {
     menuLinks.forEach(el => {
@@ -34,3 +46,55 @@ menuLinks.forEach(el => {
     e.currentTarget.classList.add('chosen');
   })
 })
+
+
+
+menu.addEventListener('click', event => {
+  if (event.target.classList.contains('list-container')) {
+    return;
+  }
+  event.preventDefault();
+  let top;
+
+  switch (event.target.dataset.id) {
+    case 'showcase':
+      top = showcaseSection.offsetTop;
+      break;
+    case 'work':
+      top = workSection.offsetTop;
+      break;
+    case 'mission':
+      top = missionSection.offsetTop;
+      break;
+    case 'partners':
+      top = partnersSection.offsetTop;
+      break;
+    case 'reviews':
+      top = reviewsSection.offsetTop;
+      break;
+    case 'team':
+      top = teamSection.offsetTop;
+      break;
+    case 'contacts':
+      top = contactsSection.offsetTop;
+      break;
+  }
+  console.log(top);
+  window.scrollTo({
+    top: top - 85,
+    behavior: 'smooth'
+  })
+})
+
+
+
+let sticky = header.offsetTop;
+
+window.addEventListener('scroll', () => {
+  
+  if (window.pageYOffset > sticky) {
+    header.classList.add('sticky');
+  } else {
+    header.classList.remove('sticky');
+  }
+});
